71fc8eb73eea78022d0bb961b42690ecba11160c
Le-Thinh <lqt9112003@gmail.com>
2025-04-19 17:06:50 +0700

    UPDATE: apikey.service

--- src/services/apikey.service.js
+++ src/services/apikey.service.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const apiKeyModel = require("../models/apiKey.model");
+const apiKeyModel = require("../models/apikey.model");
 const crypto = require("crypto");
 
 const createAPIKey = async () => {
